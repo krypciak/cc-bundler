92cb644d0c117a570d4577f4b1eef6e03b2b80a0
krypek <krypek@tuta.io>
2025-07-25 21:12:15 +0200

    Split http server implementation to a seperate file

---
+++ src/http-server/http-module.ts
@@ -0,0 +1,97 @@
+import type { InputLocations } from 'ccmoddb/build/src/types'
+import type { IncomingMessage, ServerResponse } from 'http'
+import type { createServer } from 'http-server'
+import NodeFetchCache, { MemoryCache } from 'node-fetch-cache'
+
+let allowedDbs: string[] = []
+export function setAllowedDbs(dbs: string[]) {
+    allowedDbs = dbs
+}
+
+const fetchWithCache = NodeFetchCache.create({
+    shouldCacheResponse: response => response.ok,
+    cache: new MemoryCache({
+        ttl: 1000 * 60 * 3, // 3 days
+    }),
+})
+
+async function fetchData(url: string): Promise<Uint8Array> {
+    const resp = await fetchWithCache(url)
+    const data = new Uint8Array(await resp.arrayBuffer())
+
+    return data
+}
+
+const updateInputLocationsEveryMs = 1000 * 5 // 1000 * 60 * 60 // hour
+let lastInputLocationsFetched = 0
+const validUrlSet: Set<string> = new Set()
+
+async function addAllowedDb(url: string) {
+    validUrlSet.add(`${url}/npDatabase.min.json`)
+
+    const inputLocations: InputLocations = await (await fetch(`${url}/input-locations.json`)).json()
+    for (const { url } of inputLocations) {
+        validUrlSet.add(url)
+    }
+}
+
+export async function updateValidUrlSet() {
+    validUrlSet.clear()
+    await Promise.all(allowedDbs.map(db => addAllowedDb(db)))
+    lastInputLocationsFetched = Date.now()
+}
+
+async function checkUpdate() {
+    if (lastInputLocationsFetched > Date.now() - updateInputLocationsEveryMs) return
+    await updateValidUrlSet()
+}
+
+function checkUrl(url: string | undefined): url is string {
+    if (!url) return false
+
+    return validUrlSet.has(url)
+}
+
+async function sha256(data: Uint8Array): Promise<string> {
+    const hashBuffer = await crypto.subtle.digest('SHA-256', data)
+    const hashArray = Array.from(new Uint8Array(hashBuffer))
+    const result = hashArray.map(b => b.toString(16).padStart(2, '0')).join('')
+    return result
+}
+
+export type HandleFunction = NonNullable<NonNullable<Parameters<typeof createServer>[0]>['before']>[number]
+
+export const handleFunction: HandleFunction = async (req: IncomingMessage, res: ServerResponse) => {
+    const url = req.url ?? ''
+
+    try {
+        await checkUpdate()
+
+        if (url.startsWith('/modDownload')) {
+            const matches = url.match(/\?url=(.+)/)
+            const modUrl = matches?.[1]
+
+            if (!checkUrl(modUrl)) {
+                res.writeHead(403, {})
+                res.end()
+
+                return
+            }
+
+            const data = await fetchData(modUrl)
+            const etag = await sha256(data)
+
+            res.writeHead(200, {
+                'Content-Type': 'application/zip',
+                Etag: etag,
+            })
+            res.write(data)
+            res.end()
+        } else {
+            res.emit('next')
+        }
+    } catch (e) {
+        console.error(e)
+        res.emit('next')
+    }
+}
---
+++ src/http-server/http-server.ts
@@ -0,0 +1,23 @@
+import { createServer } from 'http-server'
+import { handleFunction, setAllowedDbs, updateValidUrlSet } from './http-module.ts'
+
+export async function startHttpServer() {
+    setAllowedDbs([
+        'https://raw.githubusercontent.com/CCDirectLink/CCModDB/stable',
+        'https://raw.githubusercontent.com/CCDirectLink/CCModDB/testing',
+    ])
+    await updateValidUrlSet()
+
+    const httpServer = createServer({
+        root: './dist',
+        cache: 60 * 60 * 24,
+        cors: true,
+        showDotfiles: false,
+        showDir: 'false',
+        before: [handleFunction],
+    })
+    const port = 8080
+    console.log('http server listening to', port)
+    httpServer.listen(port)
+}
+startHttpServer()
