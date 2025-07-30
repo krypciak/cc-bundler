31eb69de92c521749e2619ea28c5e70b62eedfec
krypek <krypek@tuta.io>
2025-07-30 21:17:25 +0200

    Fix audio-warning-fix

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import * as fsProxy from './fs-proxy'
 import { requireFix } from './nwjs-fix'
-import { audioWarningFix } from './audio-warning-fix'
 import { initLoadScreen } from './ui'
 import './localstoarge-default'
 
@@ -8,11 +7,17 @@ import { addFetchHandler } from '../../ccloader3/packages/core/src/service-worke
 import { checkAutorun } from './autorun'
 
 async function setup() {
+    if (!navigator.serviceWorker) {
+        storageInfoLabel.innerHTML = 'Service Workers not supported! <br> Cannot continue'
+        return
+    }
+
     if (navigator.serviceWorker.controller) {
         initLoadScreen()
         requireFix()
 
         await fsProxy.preloadInit()
+        await window.update()
 
         checkAutorun()
     } else {
@@ -28,6 +33,4 @@ export async function run() {
 
     const modloader = await import('../../ccloader3/packages/core/src/modloader')
     await modloader.boot()
-
-    audioWarningFix()
 }
--- src/runtime-mod.ts
+++ src/runtime-mod.ts
@@ -48,4 +48,39 @@ function prestart() {
             return this.parent(data)
         },
     })
+
+    function audioWarningFix() {
+        let interacted = false
+
+        function onInteraction() {
+            if (!interacted) {
+                interacted = true
+                // @ts-expect-error
+                ig.music?.resume()
+            }
+        }
+        const dom = document.getElementById('game')!
+        dom.addEventListener('mousedown', () => {
+            onInteraction()
+        })
+        dom.addEventListener('touchstart', () => {
+            onInteraction()
+        })
+
+        ig.SoundManager.inject({
+            update() {
+                if (this.context?.context?.state == 'suspended' && !interacted) return
+                this.parent()
+            },
+        })
+
+        const WebAudioBufferGain = ig.WebAudioBufferGain.prototype as ig.WebAudioBufferGain
+        const origPlay = WebAudioBufferGain.play
+        WebAudioBufferGain.play = function (when, offset) {
+            if (!interacted) return
+            return origPlay.call(this, when, offset)
+        }
+    }
+
+    audioWarningFix()
 }
