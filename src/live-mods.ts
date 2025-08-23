d01cd38e736c6cc3ab9a35a1f61da160bce82a19
krypek <krypek@tuta.io>
2025-08-23 19:56:20 +0200

    Add live mods fetching indicator

--- src/live-mods.ts
+++ src/live-mods.ts
@@ -1,9 +1,12 @@
 import { copyFiles } from './upload-processing'
 import { getUint8Array } from './utils'
 import { fs } from './fs/opfs'
+import { updateUploadStatusLabel } from './ui'
 
 export async function updateLiveMods() {
     try {
+        updateUploadStatusLabel('fetching live mods...')
+
         const resp = await fetch('/liveModUpdate?id=list')
         if (resp.status !== 200) throw new Error(`bad status: ${resp.status}`)
         const list: string[] = await resp.json()
@@ -12,15 +15,27 @@ export async function updateLiveMods() {
         const exitsArr = await Promise.all(pathList.map(({ path }) => fs.promises.exists(path)))
         pathList = pathList.filter(({ id }, i) => localStorage[`modEnabled-${id}`] == 'true' || !exitsArr[i])
 
+        let fetched = -1
+        function updateLabel() {
+            updateUploadStatusLabel(`fetching live mods`, ++fetched, pathList.length)
+        }
+        updateLabel()
+
         const files = await Promise.all(
-            pathList.map(async ({ id, path }) => ({
-                path,
-                data: getUint8Array(await fetch(`/liveModUpdate?id=${id}`)),
-            }))
+            pathList.map(async ({ id, path }) => {
+                const ret = {
+                    path,
+                    data: getUint8Array(await fetch(`/liveModUpdate?id=${id}`)),
+                }
+                updateLabel()
+                return ret
+            })
         )
         const fileList = files.map(({ path, data }) => ({ path, uint8Array: () => data }))
         await copyFiles(fileList, false)
     } catch (e) {
         console.error('updateLiveMods error:', e)
+    } finally {
+        updateUploadStatusLabel('')
     }
 }
