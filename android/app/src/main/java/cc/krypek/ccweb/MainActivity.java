59bd7da082e9d79c1ccbc827a61f23e18b535334
krypek <krypek@tuta.io>
2025-09-22 21:13:30 +0200

    Add capacitor for android app building

---
+++ android/app/src/main/java/cc/krypek/ccweb/MainActivity.java
@@ -0,0 +1,5 @@
+package cc.krypek.ccweb;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'cc.krypek.ccweb',
+  appName: 'CrossCode',
+  webDir: 'dist'
+};
+
+export default config;
