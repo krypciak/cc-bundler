774e5ba9061405acac90c770c260b123b53e27d5
krypek <krypek@tuta.io>
2025-09-25 18:48:22 +0200

    Format capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,9 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from '@capacitor/cli'
 
 const config: CapacitorConfig = {
-  appId: 'cc.krypek.ccweb',
-  appName: 'CrossCode',
-  webDir: 'dist'
-};
+    appId: 'cc.krypek.ccweb',
+    appName: 'CrossCode',
+    webDir: 'dist',
+}
 
-export default config;
+export default config
