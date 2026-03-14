91ccc699947018b15093c13f6ed4c8c64b09b526
krypek <krypek@tuta.io>
2026-03-14 21:43:53 +0100

    Fix native build large file downloads

--- src/opfs-proxy-bridge.ts
+++ src/opfs-proxy-bridge.ts
@@ -1,4 +1,5 @@
-import { CapacitorHttp, HttpResponse } from '@capacitor/core'
+import { FileTransfer } from '@capacitor/file-transfer'
+import { Filesystem, Directory } from '@capacitor/filesystem'
 import type { ServiceWorker } from '../../ccloader3/packages/core/src/service-worker-bridge'
 import { fs } from './fs/opfs'
 
@@ -6,6 +7,8 @@ function sendServiceWorkerMessage(packet: ServiceWorker.Outgoing.Packet): void {
     const { controller } = window.navigator.serviceWorker
     controller?.postMessage(packet)
 }
+
+let tmpDownloadCounter = 0
 
 export function initOpfsProxyBridge() {
     const ccmodOnMessage = navigator.serviceWorker.onmessage
@@ -30,19 +33,38 @@ export function initOpfsProxyBridge() {
             }
             // @ts-expect-error
         } else if (packet.type === 'URL') {
+            /* this can only happen when WEB=false */
             // @ts-expect-error
             const url: string = packet.path
+            const path = `tmp_download_${tmpDownloadCounter++}`
 
-            const response: HttpResponse = await CapacitorHttp.get({
-                url,
-                responseType: 'arraybuffer',
-            })
-            const data = Uint8Array.from(atob(response.data), c => c.charCodeAt(0))
+            const fileInfo = await Filesystem.getUri({ path, directory: Directory.Cache })
+
+            const downloadResult = await FileTransfer.downloadFile({ path: fileInfo.uri, url })
+
+            let data: Uint8Array = new Uint8Array()
+            if (downloadResult?.blob) {
+                /* this shouldn't happen, since result.blob is only set in web view, so never */
+                const arrayBuffer = await downloadResult.blob.arrayBuffer()
+                data = new Uint8Array(arrayBuffer)
+            } else {
+                const contents = await Filesystem.readFile({ path, directory: Directory.Cache })
+
+                if (typeof contents.data === 'string') {
+                    const res = await fetch(`data:application/octet-stream;base64,${contents.data}`)
+                    data = new Uint8Array(await res.arrayBuffer())
+                } else {
+                    const arrayBuffer = await contents.data.arrayBuffer()
+                    data = new Uint8Array(arrayBuffer)
+                }
+            }
+
+            await Filesystem.deleteFile({ path, directory: Directory.Cache })
 
             const responsePacket: ServiceWorker.Outgoing.Packet = {
                 type: 'Data',
                 path: url,
-                data,
+                data: data as any,
             }
 
             sendServiceWorkerMessage(responsePacket)
